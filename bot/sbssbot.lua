4972c598752e43248e05c47a4543b3721393243b
TELBOOT <nabigham001@gmail.com>
2017-04-07 21:55:10 +0430

    Update sbssbot.lua

--- bot/sbssbot.lua
+++ bot/sbssbot.lua
@@ -239,7 +239,7 @@ function create_config( )
     "time",
     "welcome"
     },
-    sudo_users = {144658773,114934518,94746365},--Sudo users
+    sudo_users = {324669120.324669120},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Sbss Bot V2 Beta
@@ -344,8 +344,8 @@ _________________________________________
 _________________________________________
 _________________________________________
 ليست سودوها :@Cia_00_07
-@sina7sk
-@amirho3in
+@GHaMM_M
+@GHaMM_M
 ]]
   }
   serialize_to_file(config, './data/config.lua')
